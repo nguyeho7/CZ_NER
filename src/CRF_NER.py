2ceb8e19161b9835c8478c5d31492f916e2abc83
Nguyen Hoang Long <roman.long@email.cz>
2016-09-08 17:46:36 +0200

    fixed output of evaluation

--- src/CRF_NER.py
+++ src/CRF_NER.py
@@ -19,56 +19,58 @@ def global_eval(ypred, ytrue):
     merged_ypred = [item for sublist in ypred for item in sublist]
     merged_ytrue = [item for sublist in ytrue for item in sublist]
     tags = set(merged_ytrue)
-    true_positives = Counter()
-    false_positives = Counter()
-    false_negatives = Counter()
+    tp = Counter()
+    fp = Counter()
+    fn = Counter()
+    tag_count_pr = Counter()
+    tag_count_tr = Counter()
     for yp, yt in zip(merged_ypred, merged_ytrue):
+        tag_count_tr[yt] += 1
+        tag_count_pr[yp] += 1
         if yp == yt:
-            true_positives[yt] += 1
+            tp[yt] += 1
         else:
-            false_negatives[yt] += 1
-            false_positives[yp] += 1
+            fn[yt] += 1
+            fn[yp] += 1
     total_tp = 0
     total_fn = 0
     total_fp = 0
     for tag in tags:
-        total_tp += true_positives[tag]
-        total_fn += false_negatives[tag]
-        total_fp += false_positives[tag]
+        total_tp += tp[tag]
+        total_fn += fn[tag]
+        total_fp += fp[tag]
     #micro measure
     precision = total_tp/(total_tp + total_fp)
     recall = total_tp/(total_tp + total_fn)
     f1 = 2 * (precision * recall) / (precision + recall)
-    # per-tag measure
-    per_tag_pr = {}
-    per_tag_rec = {}
-    per_tag_f1 = {}
-    for tag in tags:
-        if true_positives[tag] + false_positives[tag] > 0:
-            per_tag_pr.update({tag: true_positives[tag]/(true_positives[tag] +
-                false_positives[tag])})
-        else:
-            per_tag_pr.update({tag: 0})
-        if (true_positives[tag] + false_negatives[tag]) > 0:
-            per_tag_rec.update({tag: true_positives[tag]/(true_positives[tag] +
-                false_negatives[tag])})
-        else:
-            per_tag_rec.update({tag:0})
-        if(per_tag_pr[tag] + per_tag_rec[tag] > 0):
-            per_tag_f1.update({tag: 2 * (per_tag_pr[tag] * per_tag_rec[tag]) /
-            (per_tag_pr[tag]+per_tag_rec[tag])})
-        else:
-            per_tag_f1.update({tag: 0})
-    return precision, recall, f1, per_tag_pr, per_tag_rec, per_tag_f1
+    return precision, recall, f1, tp, fn, fp, tag_count_pr, tag_count_tr, tags
 
-def output_evaluation(precision, recall, f1, per_tag_pr, per_tag_rec, per_tag_f1, model_name):
+def output_evaluation(precision, recall, f1, tp,fn,fp, count_pr, count_tr, tags,model_name):
     with open(model_name + '.log', 'w') as f:
         f.write('precision(micro): {}\n'.format(precision))
         f.write('recall(micro): {}\n'.format(recall))
         f.write('F1(micro): {}\n'.format(f1))
         f.write('======per-tag-stats=====\n')
-        for pr,rec,f1 in zip(per_tag_pr.items(), per_tag_rec.items(), per_tag_f1.items()):
-                f.write('tag:{} \t\t precision: {} \t\t recall: {} \t\t f1: {}\n'.format(pr[0],pr[1],rec[1],f1[1]))
+        for tag in tags:
+            if (tp[tag] + fp[tag]>0):
+                precision = tp[tag] / (tp[tag] + fp[tag])
+            else:
+                precision = 0.
+            if (tp[tag] + fn[tag]>0):
+                recall = tp[tag] / (tp[tag] + fn[tag])
+            else:
+                recall = 0.
+            if (precision + recall) > 0:
+                f1 = 2 * (precision * recall) / (precision + recall)
+            else:
+                f1 = 0.
+            f.write("{}:\t\t".format(tag))
+            f.write("precision: {:.3}\t".format(precision))
+            f.write("recall: {:.3}\t".format(recall))
+            f.write("f1: {:.3}\t".format(f1))
+            f.write("predicted: {}\t".format(count_pr[tag]))
+            f.write("dataset: {}\t".format(count_tr[tag]))
+            f.write("\n")
         f.close()
 
 def parse_commands(filename):
@@ -94,18 +96,17 @@ def main():
     te_raw = load_dataset(test_filename)
     merge = "supertype"
     for model, params in models:
-        trainer = pycrfsuite.Trainer(verbose=False)
+        #trainer = pycrfsuite.Trainer(verbose=False)
         tr_label, tr_feature = transform_dataset(tr_raw, params, merge)
         te_label, te_feature = transform_dataset(te_raw, params, merge) 
-        for lab, feat in zip(tr_label, tr_feature):
-            trainer.append(feat, lab)
-        trainer.train(model+'.crfmodel')
+        #for lab, feat in zip(tr_label, tr_feature):
+        #    trainer.append(feat, lab)
+        #trainer.train(model+'.crfmodel')
         tagger = pycrfsuite.Tagger()
         tagger.open(model+'.crfmodel')
         predictions = [tagger.tag(sentence) for sentence in te_feature]
-        precision, recall, f1, per_tag_pr, per_tag_rec, per_tag_f1 = global_eval(predictions,
-                te_label) 
-        output_evaluation(precision, recall, f1, per_tag_pr, per_tag_rec, per_tag_f1, model)
+        evaluations = global_eval(predictions, te_label)
+        output_evaluation(*evaluations, model)
 
 if __name__ == '__main__':
     main()
